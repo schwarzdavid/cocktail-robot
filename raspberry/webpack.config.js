544d352a88754ca87542925282e5e8c418f65cc0
David Schwarz <schwarz.david10@gmail.com>
2020-03-09 00:16:30 +0100

    Buildtools setup (hopefully) finished

--- raspberry/webpack.config.js
+++ raspberry/webpack.config.js
@@ -1,4 +1,80 @@
-const mainConfig = require('./webpack.main');
-const rendererConfig = require('./webpack.renderer');
+const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = [mainConfig, rendererConfig];
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = {
+	entry: path.join(__dirname, 'src/renderer/index.ts'),
+	target: 'electron-renderer',
+	output: {
+		path: path.join(__dirname, 'dist/renderer/')
+	},
+	devtool: isDev ? 'source-map' : false,
+	mode: isProd ? 'production' : 'development',
+	resolve: {
+		alias: {
+			'~': path.join(__dirname, 'node_modules')
+		},
+		extensions: ['*', '.vue', '.js', '.json', '.ts']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				use: ['vue-loader']
+			},
+			{
+				test: /\.ts$/,
+				use: ['ts-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					isProd ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+					'css-loader',
+					'resolve-url-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							sourceMapContents: false
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|webp)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						outputPath: 'img/',
+						limit: 8192
+					}
+				}]
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						outputPath: 'fonts/'
+					}
+				}]
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src/renderer/index.ejs')
+		})
+	]
+};
+
+module.exports = config;
+
--- raspberry/webpack.main.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge');
-const {isProd, isDev, common} = require('./webpack.common');
-
-const config = merge(common, {
-
-});
-
-module.exports = config;
-
