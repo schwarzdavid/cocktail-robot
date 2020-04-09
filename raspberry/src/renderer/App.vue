571a0638ae7b1b13de815bdfd9fba791276dcd95
David Schwarz <schwarz.david10@gmail.com>
2020-04-09 08:14:55 +0200

    3d files added & raspberry code updated

--- raspberry/src/renderer/App.vue
+++ raspberry/src/renderer/App.vue
@@ -1,19 +1,30 @@
 <template>
-	<h1>Hallo i bims</h1>
+	<div>
+		<Menu />
+		<div>
+			<h1>Leiwand</h1>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
 	import Component from 'vue-class-component';
+	import Menu from './components/content/Menu.vue';
 
-	@Component
+	@Component({
+		components: {
+			Menu
+		}
+	})
 	export default class App extends Vue {
 
 	}
 </script>
 
+<style lang="scss" src="@css/main.scss"></style>
 <style lang="scss">
 	html, body {
-		color: $color;
+
 	}
 </style>
---
+++ raspberry/src/renderer/components/content/StatusBar.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="statusbar d-flex">
+		<span class="mdi mdi-wifi"></span>
+		<span class="mdi mdi-swap-vertical"></span>
+		<span class="mdi mdi-check-circle-outline"></span>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import Component from "vue-class-component";
+
+	@Component
+	export default class StatusBar extends Vue {
+
+	}
+</script>
+
+<style lang="scss">
+	.statusbar {
+
+	}
+</style>
